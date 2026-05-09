4546564eccdc576cb786eb697c9a669370be37d5
Maryam-ML <maryamg786786@gmail.com>
2026-05-09 17:16:09 +0500

    .

--- dfadetect/agnostic_datasets/fakeavceleb_dataset.py
+++ dfadetect/agnostic_datasets/fakeavceleb_dataset.py
@@ -33,10 +33,8 @@ FAKEAVCELEB_KFOLD_SPLIT = {
 class FakeAVCelebDataset(SimpleAudioFakeDataset):
 
     audio_folder = "/kaggle/input/datasets/rabiaanum/fakeavceleb"
-    audio_extension = ".mp3"
-    metadata_file = Path(audio_folder) / "meta_data_selected_methods.csv
-
-"
+    audio_extension = ".flacc"
+    metadata_file = Path(audio_folder) / "meta_data_selected_methods.csv"
     subsets = ("train", "dev", "eval")
 
     def __init__(self, path, fold_num=0, fold_subset="train", transform=None):
